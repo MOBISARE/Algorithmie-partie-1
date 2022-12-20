5786b28295e59c9b5a277afe61ba6e4f746d3f7b
MOBISARE <mosellemarieluc@gmail.com>
2022-12-20 15:50:16 +0100

    Initialisation du projet

--- src/Toto.java
+++ src/Toto.java
@@ -9,38 +9,32 @@ import java.io.File;
 public class Toto{
 
     public static String FileToString(String filename){
-	try{
-	    
-	File file = new File(Objects.requireNonNull(Toto.class.getResource(filename)).toURI());
-	FileInputStream f = new FileInputStream(file);
-	byte[] data = new byte[(int) file.length()];
-	f.read(data);
-	f.close();
-	
-	return new String(data, "UTF-8");
-	}
-	 catch(Exception ex)
-	     {
-		 ex.printStackTrace();
-		 return null;
-	     }       
+		try{
+
+			File file = new File(Objects.requireNonNull(Toto.class.getResource(filename)).toURI());
+			FileInputStream f = new FileInputStream(file);
+			byte[] data = new byte[(int) file.length()];
+			f.read(data);
+			f.close();
+
+			return new String(data, "UTF-8");
+	} catch(Exception ex) {
+		 	ex.printStackTrace();
+		 	return null;
+		}
     }
 
     public static void main(String[] args){
-	// String s = "x=1;y=x*2;z=x-y;x=y+z";
-	Vector<Instruction> x = Parser.parse(Objects.requireNonNull(FileToString("prog1")));
-	for(Instruction i: x){
-	    if (i instanceof Assign){
-		Assign a = (Assign) i;
-		System.out.println("La variable " + a.lhs + " reçoit la valeur " + a.rhs);
-	    }
-	    else{		
-		AssignOperator a = (AssignOperator) i;
-		System.out.println("On effectue l'opération " + a.op + " aux valeurs " + a.t0  + " et " + a.t1 + " et on stocke le résultat dans " + a.lhs);
-	    }
-
-	    
-	}
+		// String s = "x=1;y=x*2;z=x-y;x=y+z";
+		Vector<Instruction> x = Parser.parse(Objects.requireNonNull(FileToString("prog1")));
+		for(Instruction i: x){
+			if (i instanceof Assign) {
+			Assign a = (Assign) i;
+			System.out.println("La variable " + a.lhs + " reçoit la valeur " + a.rhs);
+			} else {
+			AssignOperator a = (AssignOperator) i;
+			System.out.println("On effectue l'opération " + a.op + " aux valeurs " + a.t0  + " et " + a.t1 + " et on stocke le résultat dans " + a.lhs);
+			}
+		}
     }
-
 }
