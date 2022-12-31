e8d584de11d292d8761ed6ab51cf6dc36d536251
MOBISARE <mosellemarieluc@gmail.com>
2022-12-31 23:08:34 +0100

    Mise à jour du Main

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ public class Main {
 
     public static void main(String[] args){
 		// String s = "x=1;y=x*2;z=x-y;x=y+z";
-		Evaluateur evaluateur = new Evaluateur("prog1");
-        System.out.println(evaluateur.evaluate());
-        System.out.println(evaluateur.simplifyEvaluate());
+		Evaluateur evaluateur1 = new Evaluateur("prog1");
+        Evaluateur evaluateur2 = new Evaluateur("opt_prog.txt");
+        System.out.println("Question 1\n" + evaluateur1.evaluate());
+        System.out.println("Question 3\n voir fichier créé ressources/opt_prog.txt");
+        evaluateur1.abstractEvaluate();
+        System.out.println("Question 4\n" + evaluateur1.simplifyEvaluate());
+        System.out.println("Question 5\nmodifier le fichier ressources/opt_prog.txt " +
+                "pour comparer si on obtient ou non le même résultat\n"
+                + (evaluateur1.simplifyEvaluate() == evaluateur2.simplifyEvaluate()));
 
-        evaluateur.abstractEvaluate();
     }
 }
